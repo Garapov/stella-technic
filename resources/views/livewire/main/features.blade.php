df58316a7076b1f9ccfbefb69d492656cef2fcb6
Ruslan Garapov <ruslangarapov1994@gmail.com>
2024-10-15 16:58:48 +0300

    Верстка блоков на главной

---
+++ resources/views/livewire/main/features.blade.php
@@ -0,0 +1,54 @@
+<section class="text-gray-600 body-font bg-gray-200 dark:bg-gray-700">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="text-center mb-10">
+            <h1 class="md:text-4xl text-3xl font-medium text-center title-font text-gray-900 dark:text-gray-200 mb-2">
+                Более 30 лет</h1>
+            <p class="text-base dark:text-gray-400 leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">На рынке оборудования и
+                хранения для складов</p>
+        </div>
+        <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <x-fas-border-all class="w-8 h-8 text-indigo-500 mr-4" />
+                    <span class="title-font font-medium">Все для склада в одном месте</span>
+                </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <x-fas-border-all class="w-8 h-8 text-indigo-500 mr-4" />
+                    <span class="title-font font-medium">Собственное производство</span>
+                </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <x-fas-border-all class="w-8 h-8 text-indigo-500 mr-4" />
+                    <span class="title-font font-medium">Скидки до 30% на оптовые заказы</span>
+                </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <x-fas-border-all class="w-8 h-8 text-indigo-500 mr-4" />
+                    <span class="title-font font-medium">Сертифицированная продукция</span>
+                </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <x-fas-border-all class="w-8 h-8 text-indigo-500 mr-4" />
+                    <span class="title-font font-medium">Более 50 городов РФ + Казахстан и Беларусь</span>
+                </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <x-fas-border-all class="w-8 h-8 text-indigo-500 mr-4" />
+                    <span class="title-font font-medium">Pack Truffaut Blue</span>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="grid grid-cols-4">
+            
+        </div>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,18 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="grid grid-cols-3 gap-4 mx-auto max-w-screen-xl">
+        <div class="col-span-2">
+            @livewire('main.slider')
+        </div>
+        <div class="flex flex-col gap-4">
+            <div>
+                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
             </div>
         </div>
     </div>
+
+    @livewire('main.brands')
+    @livewire('main.features')
 </x-guest-layout>
