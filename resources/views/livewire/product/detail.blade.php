22266b9b7f0c859a05d09dacdd4f4e4894d0d8de
Ruslan Garapov <ruslangarapov1994@gmail.com>
2024-12-08 21:44:34 +0500

    добавлена страница товара

--- resources/views/livewire/product/detail.blade.php
+++ resources/views/livewire/product/detail.blade.php
@@ -3,6 +3,7 @@
     product: @js($product),
     gallery: @js($gallery),
     selectedVariation: null,
+    activeTab: 'description',
     init() {
         this.product.variants.map(variant => {
             if (variant.is_default) {
@@ -25,6 +26,16 @@
             count: this.cart_quantity,
             variation: this.selectedVariation
         });
+    },
+    decrease() {
+        if (this.cart_quantity < 2) return;
+        this.cart_quantity--;
+    },
+    increase() {
+        this.cart_quantity++;
+    },
+    validateCount() {
+        if (this.cart_quantity < 1) this.cart_quantity = 1;
     },
     get productData() {
         if (this.selectedVariation) return this.selectedVariation;
@@ -66,9 +77,9 @@
                 </svg>
             </a>
 
-            <a href="#" title=""
+            <button title=""
                 class="text-white mt-4 sm:mt-0 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex items-center justify-center"
-                role="button">
+                role="button" @click="selectedVariation ? addVariationToCart() : addToCart()">
                 <svg class="w-5 h-5 -ms-2 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                     width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -77,16 +88,16 @@
                 </svg>
 
                 Add to cart
-            </a>
+            </button>
 
             <div class="relative flex items-center max-w-[8rem]">
-                <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none" @click="decrease">
                     <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
                     </svg>
                 </button>
-                <input type="text" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation" class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
-                <button type="button" id="increment-button" data-input-counter-increment="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                <input type="number" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation" class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="999" x-model="cart_quantity" @change="validateCount" />
+                <button type="button" id="increment-button" data-input-counter-increment="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none" @click="increase">
                     <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                     </svg>
@@ -120,8 +131,62 @@
         <hr class="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
         
 
-        <p class="mb-6 text-gray-500 dark:text-gray-400">
-            {!! str($product->description)->sanitizeHtml() !!}
-        </p>
+        
+    </div>
+
+    <div class="col-span-full">
+        <!-- Tabs -->
+        <div class="border-b border-gray-200 dark:border-gray-700">
+            <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                <li class="mr-2">
+                    <button 
+                        @click="activeTab = 'description'" 
+                        :class="{'text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500': activeTab === 'description',
+                                'border-transparent hover:text-gray-600 hover:border-gray-300': activeTab !== 'description'}"
+                        class="inline-flex items-center justify-center p-4 border-b-2 rounded-t-lg group">
+                        Описание
+                    </button>
+                </li>
+                <li class="mr-2">
+                    <button 
+                        @click="activeTab = 'specifications'" 
+                        :class="{'text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500': activeTab === 'specifications',
+                                'border-transparent hover:text-gray-600 hover:border-gray-300': activeTab !== 'specifications'}"
+                        class="inline-flex items-center justify-center p-4 border-b-2 rounded-t-lg group">
+                        Параметры
+                    </button>
+                </li>
+                <!-- ... остальные табы ... -->
+            </ul>
+        </div>
+
+        <div class="p-4">
+            <!-- Description Tab -->
+            <div x-show="activeTab === 'description'" class="text-gray-500 dark:text-gray-400">
+                {!! str($product->description)->sanitizeHtml() !!}
+            </div>
+
+            <!-- Specifications Tab -->
+            <div x-show="activeTab === 'specifications'" class="text-gray-500 dark:text-gray-400">
+                <dl class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    @foreach ($product->variants as $variant)
+                        <div class="flex justify-between">
+                            <dt class="font-medium text-gray-900 dark:text-white">{{ $variant->param->productParam->name }}</dt>
+                            <dd>{{ $variant->param->title }}</dd>
+                        </div>
+                    @endforeach
+                    
+                    <!-- Базовые характеристики товара, если они есть -->
+                    @if($product->specifications)
+                        @foreach ($product->specifications as $name => $value)
+                            <div class="flex justify-between">
+                                <dt class="font-medium text-gray-900 dark:text-white">{{ $name }}</dt>
+                                <dd>{{ $value }}</dd>
+                            </div>
+                        @endforeach
+                    @endif
+                </dl>
+            </div>
+        </div>
     </div>
 </div>
