133ba50695651889fbedfe6fa58777851e29d9e5
Stella-tech server <info@stella-technic.ru>
2025-02-25 03:00:19 +0300

    Переделан каталог товаров в шапке

--- resources/views/livewire/general/category.blade.php
+++ resources/views/livewire/general/category.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('client.catalog', $category->slug) }}" wire:navigate class="glide__slide p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-900 dark:border-gray-700 relative overflow-hidden h-auto flex flex-col justify-between gap-8">
+<div class="glide__slide p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-900 dark:border-gray-700 relative overflow-hidden h-auto flex flex-col justify-between gap-8 h-full">
     <div class="w-[45%] absolute top-[-10px] right-[-10px] opacity-10 text-yellow-900 dark:opacity-20">
         {{ svg($category->icon) }}
     </div>
@@ -7,16 +7,22 @@
             {{ count($category->products) . ' ' . (count($category->products) % 10 === 1 && count($category->products) % 100 !== 11 ? 'товар' : (count($category->products) % 10 >= 2 && count($category->products) % 10 <= 4 && (count($category->products) % 100 < 10 || count($category->products) % 100 >= 20) ? 'товара' : 'товаров')) }}
         </span>
         <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $category->title }}</h5>
-        @if ($category->description)
-            <!-- <p class="text-gray-500 dark:text-gray-400 max-w-[50%]">{{ $category->description }}</p> -->
+        @if ($category->categories) 
+            <ul class="flex flex-col gap-0.5">
+                @foreach ($category->categories as $subcategory)
+                    @if ($subcategory->products->count() > 0)
+                        <li><a href="{{ route('client.catalog', $subcategory->slug) }}" wire:navigate class="text-gray-500 hover:text-gray-900 dark:hover:text-white text-xs flex items-center gap-2">{{ $subcategory->title }} <span class="bg-yellow-100 text-yellow-800 text-sm font-medium px-1.5 py-0.5 rounded-sm dark:bg-yellow-900 dark:text-yellow-300 text-xs">{{ $subcategory->products->count() }}</span></a></li>
+                    @endif
+                @endforeach
+            </ul>
         @endif
     </div>
-    <div class="flex items-center justify-between">
+    <a href="{{ route('client.catalog', $category->slug) }}" wire:navigate class="flex items-center justify-between">
         <span class="inline-flex items-center text-yellow-800 hover:underline dark:text-yellow-300">
             Все товары
             <x-carbon-arrow-up-right class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" />
         </span>
         <div class="text-lg font-bold text-gray-900 dark:text-white">От {{ $category->minProductPrice() }} ₽</div>
-    </div>
-</a>
+    </a>
+</div>
 
--- resources/views/livewire/general/header/catalog.blade.php
+++ resources/views/livewire/general/header/catalog.blade.php
@@ -1,10 +1,62 @@
-<div x-data="dropdown" @click.outside="close">
+<div x-data="{
+    isOpened: false,
+    selectedCategory: {{ $categories->first()->id }},
+    toggle() {
+        this.isOpened = !this.isOpened;
+    },
+    close() {
+        this.isOpened = false;
+    },
+    changeSelectedCategory(id) {
+        this.selectedCategory = id;
+    }
+}" @click.outside="close">
     <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="toggle">
             <x-fas-list-ul class="w-3.5 h-3.5 me-2" x-show="!isOpened" />
             <x-fas-xmark class="w-3.5 h-3.5 me-2" x-show="isOpened" />
         Каталог
     </button>
-    <div class="absolute top-full left-[50%] transform translate-x-[-50%] z-[999] hidden container overflow-auto border border-blue-gray-50 bg-white p-3 font-sans text-sm font-normal text-blue-gray-500  shadow-lg shadow-blue-gray-500/10 focus:outline-none lg:block" role="menu" x-show="isOpened" >
+    <div class="absolute top-full left-[50%] transform translate-x-[-50%] z-[999] hidden container overflow-auto border border-blue-gray-50 bg-white p-3 font-sans text-sm font-normal text-blue-gray-500  shadow-lg shadow-blue-gray-500/10 focus:outline-none lg:block rounded-lg" role="menu" x-show="isOpened" >
+        <div class="grid grid-cols-5 gap-4">
+            <ul class="flex flex-col gap-4 pr-2 max-h-80 overflow-y-auto">
+                @foreach ($categories as $category)
+                    <li>
+                        <a href="{{ route('client.catalog', $category->slug) }}" wire:navigate class="flex flex-center justify-between gap-2 text-gray-600" :class="{ 'text-red-700': selectedCategory == {{ $category->id }}}" @mouseover="changeSelectedCategory({{ $category->id }})">
+                            <div class="flex items-center gap-2">
+                                <div class="min-w-5 max-w-5">
+                                    @svg( $category->icon )
+                                </div>
+                                {{ $category->title }}
+                            </div>
+                            @if ($category->categories) 
+                                <x-heroicon-o-arrow-long-right class="min-w-4 max-w-4" />
+                            @endif
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            @foreach ($categories as $category)
+                @if (!$category->categories) 
+                    @continue
+                @endif
+                <div class="max-h-80 col-span-4"  x-show="selectedCategory == {{ $category->id }}">
+                    <div class="bg-gray-200 rounded-lg h-full overflow-y-auto">
+                        <ul class="grid grid-cols-3 gap-4 items-center p-4">
+                            @foreach ($category->categories as $subcategory)
+                                @if ($subcategory->products->count() == 0)
+                                    @continue
+                                @endif
+                                @livewire('general.category', [
+                                    'category' => $subcategory,
+                                ], key($subcategory->id))
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    {{-- <div class="absolute top-full left-[50%] transform translate-x-[-50%] z-[999] hidden container overflow-auto border border-blue-gray-50 bg-white p-3 font-sans text-sm font-normal text-blue-gray-500  shadow-lg shadow-blue-gray-500/10 focus:outline-none lg:block" role="menu" x-show="isOpened" >
         <ul class="grid grid-cols-3 gap-y-2 outline-none outline-0" role="menuitem">
 
             @foreach ($categories as $category)
@@ -24,12 +76,10 @@
                         </div>
                     </button>
                 </a>
-                {{-- {{ $category }} --}}
                 @if ($category->categories)
                 <ul class="ml-4">
                     @foreach ($category->categories as $subcategory)
                         <li>
-                            {{-- {{ $subcategory }} --}}
                             <a href="{{ route('client.catalog', $subcategory->slug) }}" class="text-blue-gray-500 hover:underline">
                                 {{ $subcategory->title }}
                             </a>
@@ -41,5 +91,5 @@
 
             
         </ul>
-    </div>
+    </div> --}}
 </div>
