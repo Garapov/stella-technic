80c33400d7cf8e53e066728e2a24e864d59b282e
Stella-tech server <info@stella-technic.ru>
2025-06-11 02:49:52 +0300

    Адаптивные стили для детальной карточки товара

--- resources/views/client/product_detail.blade.php
+++ resources/views/client/product_detail.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <section class="py-8 bg-white md:py-10 dark:bg-gray-900 antialiased">
-        <div class="container px-4 mx-auto 2xl:px-0">
+        <div class="lg:container px-4 lg:mx-auto 2xl:px-0">
             <div class="mb-10">@livewire('general.breadcrumbs')</div>
             @livewire('product.detail', ['slug' => $product_slug])
         </div>
