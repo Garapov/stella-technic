49bb075fbbe227bf453f60ed03e9896eb8d5a5e0
Stella-tech server <info@stella-technic.ru>
2025-06-29 23:27:44 +0300

    Изменения на странице товара

--- resources/views/components/product/panel.blade.php
+++ resources/views/components/product/panel.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-4 md:sticky top-10">
+<div class="flex flex-col gap-4">
     <div class="flex flex-col gap-4 border border-blue-500 p-4 rounded-xl">
         <div class="flex items-center flex-wrap justify-between gap-4">
             <h4 class="text-slate-900 text-4xl font-semibold">{{ $variation->new_price ? Number::format($variation->new_price, 0) : Number::format($variation->getActualPrice(), 0) }} ₽</h4>
@@ -30,20 +30,29 @@
 
         <div class="flex flex-wrap items-center gap-4">
 
-            <div class="max-w-40 py-2 px-3 bg-gray-100 rounded-lg dark:bg-neutral-700">
-                <div class="flex justify-between items-center gap-x-5">
+            <div class="max-w-[110px] py-2 px-3 bg-gray-100 rounded-lg dark:bg-neutral-700">
+                <div class="flex justify-between items-center gap-x-2">
                     <div class="grow">
                         <input class="w-full p-0 bg-transparent border-0 text-gray-800 focus:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none dark:text-white" style="-moz-appearance: textfield;" type="number" aria-roledescription="Quantity field" x-model="cart_quantity" @change="validateQuantity">
                     </div>
-                    <div class="flex justify-end items-center gap-x-1.5">
-                        <button type="button" class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" tabindex="-1" aria-label="Decrease Quantity" @click="decreaseQuantity">
-                            <x-heroicon-o-minus class="shrink-0 size-3.5" />
-                        </button>
-                        <button type="button" class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" tabindex="-1" aria-label="Increase Quantity" @click="increaseQuantity">
+                    <div class="flex flex-col justify-end items-center gap-0.5">
+                        <button type="button" class="size-5 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" tabindex="-1" aria-label="Increase Quantity" @click="increaseQuantity">
                             <x-heroicon-o-plus class="shrink-0 size-3.5" />
                         </button>
+                        <button type="button" class="size-5 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" tabindex="-1" aria-label="Decrease Quantity" @click="decreaseQuantity">
+                            <x-heroicon-o-minus class="shrink-0 size-3.5" />
+                        </button>
+                        
                     </div>
                 </div>
+            </div>
+            <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center justify-center" @click="addVariationToCart()">
+                <x-fas-cart-arrow-down class="w-6 h-6 mr-2" />
+                <span class="text-md">В корзину</span>
+            </button>
+            
+            <div class="grow flex flex-col gap-2">
+                <button class="rounded-lg p-3 text-gray-500 bg-gray-100 text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" @click="$store.application.forms.buy_one_click = true">Купить в один клик</button>
             </div>
             <div class="relative" x-data="{ showTooltip: false }">
                 <button type="button"
@@ -72,13 +81,7 @@
                     <div class="absolute -bottom-1 left-1/2 h-2 w-2 -translate-x-1/2 transform rotate-45 bg-gray-900"></div>
                 </div>
             </div>
-            <div class="grow flex flex-col gap-2">
-                <button class="rounded-lg p-3 text-gray-500 bg-gray-100 text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" @click="$store.application.forms.buy_one_click = true">Купить в один клик</button>
-            </div>
-            <button type="button" class="w-full text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center justify-center" @click="addVariationToCart()">
-                <x-fas-cart-arrow-down class="w-6 h-6 mr-2" />
-                <span class="text-md">В корзину</span>
-            </button>
+            
             @if ($variation->is_constructable)
                 <a href="{{ route('client.constructor', ['variation_id' => $variation->id]) }}" class="w-full text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Редактировать в конструкторе</a>
             @endif
---
+++ resources/views/components/product/params.blade.php
@@ -0,0 +1,52 @@
+<div class="flex flex-col gap-2 md:sticky top-10">
+    <div class="parametrs">
+        @foreach($groupedParams as $paramGroup)
+            <div class="mb-6">
+                @php
+                    $activeParamName = collect(array_filter($paramGroup['values'], fn ($paramValue) => $paramValue['is_current']));
+                @endphp
+                <h3 class="text-lg sm:text-sm font-semibold text-slate-900 dark:text-white @if (count($activeParamName) < 1) hidden @endif">{{ $paramGroup['name'] }} @if ($activeParamName->first())({{$activeParamName->first()['title']}})@endif</h3>
+                <div class="flex flex-wrap gap-4 mt-2 @if (count($activeParamName) < 1) hidden @endif">
+                    @foreach($paramGroup['values'] as $value)
+                        @if($paramGroup['name'] === 'Цвет')
+                            <a href="{{ route('client.catalog', $variation->product->variants->where('id', $value['variant_id'])->first()->urlChain()) }}" wire:navigate
+                                @class([
+                                    'relative flex items-center gap-2 border rounded-full',
+                                    'border-blue-600' => $value['is_current'],
+                                    'border-slate-300 hover:border-blue-600' => !$value['is_current'] && $value['is_available'],
+                                    'border-slate-200 opacity-30' => !$value['is_available'],
+                                ])
+
+                                @if(!$value['is_available']) disabled @endif>
+                                @php
+                                    $colors = explode('|', $value['value']);
+                                @endphp
+                                <div class="relative w-8 h-8 rounded-full border @if($value['is_current']) border-blue-500 @else border-gray-300 @endif overflow-hidden">
+                                    @if(count($colors) > 1)
+                                        <div class="absolute inset-0">
+                                            <div class="h-full w-1/2 float-left" style="background-color: {{ trim($colors[0]) }}"></div>
+                                            <div class="h-full w-1/2 float-right" style="background-color: {{ trim($colors[1]) }}"></div>
+                                        </div>
+                                    @else
+                                        <div class="absolute inset-0" style="background-color: {{ trim($colors[0]) }}"></div>
+                                    @endif
+                                </div>
+                            </a>
+                        @else
+                            <a href="{{ route('client.catalog', $variation->product->variants->where('id', $value['variant_id'])->first()->urlChain()) }}"
+                                @class([
+                                    'px-2 py-2 border text-sm flex items-center justify-center shrink-0 text-xs rounded-xl dark:text-white',
+                                    'bg-blue-50 border-blue-50' => $value['is_current'],
+                                    'border-white hover:border-blue-50' => !$value['is_current'] && $value['is_available'],
+                                    'border-slate-200 opacity-30' => !$value['is_available'],
+                                    'hidden' => !$value['is_current'] && $value['is_fixed'],
+                                ]) wire:navigate >
+                                {{ $value['title'] }}
+                            </a>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
