91443b4b75ad20fe5940fa841cacee4f0209ed85
Stella-tech server <info@stella-technic.ru>
2025-09-14 23:01:08 +0300

    Структура JSON-LD для верхнего меню

--- app/Livewire/General/Header/Topmenu.php
+++ app/Livewire/General/Header/Topmenu.php
@@ -3,17 +3,84 @@
 namespace App\Livewire\General\Header;
 
 use Livewire\Component;
+use Spatie\SchemaOrg\Schema;
 
 class Topmenu extends Component
 {
     public $menu;
+    public $schema;
 
     public function mount($menu)
     {
         $this->menu = $menu;
+
+        $this->schema = $this->makeSchema()->toScript();
     }
+
+    protected function makeSchema()
+    {
+        $menu = Schema::menu()->name('Главное меню');
+
+        $sections = [];
+        $items = [];
+
+        foreach ($this->menu->menuItems as $menuItem) {
+            if ($menuItem->children && $menuItem->children->count() > 0) {
+                $sections[] = $this->makeMenuSection($menuItem);
+            } else {
+                $items[] = $this->makeMenuItem($menuItem);
+            }
+        }
+
+        if ($sections) {
+            $menu->hasMenuSection($sections);
+        }
+
+        if ($items) {
+            $menu->hasMenuItem($items);
+        }
+
+        return $menu;
+    }
+
+    protected function makeMenuItem($item)
+    {
+        return Schema::menuItem()
+            ->name($item->title)
+            ->url($item->url);
+    }
+
+    protected function makeMenuSection($item)
+    {
+        $section = Schema::menuSection()->name($item->title);
+
+        $childSections = [];
+        $childItems = [];
+
+        foreach ($item->children as $child) {
+            if ($child->children && $child->children->count() > 0) {
+                $childSections[] = $this->makeMenuSection($child);
+            } else {
+                $childItems[] = $this->makeMenuItem($child);
+            }
+        }
+
+        if ($childSections) {
+            $section->hasMenuSection($childSections);
+        }
+
+        if ($childItems) {
+            $section->hasMenuItem($childItems);
+        }
+
+        return $section;
+    }
+
+
     public function render()
     {
-        return view('livewire.general.header.topmenu');
+        return view('livewire.general.header.topmenu', [
+            'schema' => $this->schema,
+        ]);
     }
 }
--- resources/views/livewire/general/header/topmenu.blade.php
+++ resources/views/livewire/general/header/topmenu.blade.php
@@ -1,38 +1,43 @@
-<ul class="flex items-center gap-4 mt-4 font-medium lg:mt-0">
-    @foreach ($menu->menuItems as $menuItem)
-        <li>
-            @if (count($menuItem->children) > 0)
-                <span class="relative z-30" x-data="{ open: false }" @mouseover="open = true" @mouseleave="open = false" x-cloak>
-                    <span
-                    class="block py-2 pr-4 pl-3 text-sm text-slate-500 hover:text-blue-500 lg:p-0 dark:text-white cursor-pointer flex items-center gap-1"
-                    aria-current="page">{{ $menuItem->title }} <x-eva-arrow-ios-downward class="w-3 h-3" x-show="!open" /><x-eva-arrow-ios-upward class="w-3 h-3" x-show="open" /></span>
-                    <ul class="absolute top-full left-0 flex flex-col font-medium p-4 bg-white border border-blue-500 dark:bg-gray-700 dark:border-gray-600 rounded gap-1 z-60" x-show="open">
-                        @foreach ($menuItem->children as $child)
+<div>
+    @if ($schema)
+        {!! $schema !!}
+    @endif
+    <ul class="flex items-center gap-4 mt-4 font-medium lg:mt-0">
+        @foreach ($menu->menuItems as $menuItem)
+            <li>
+                @if (count($menuItem->children) > 0)
+                    <span class="relative z-30" x-data="{ open: false }" @mouseover="open = true" @mouseleave="open = false" x-cloak>
+                        <span
+                        class="block py-2 pr-4 pl-3 text-sm text-slate-500 hover:text-blue-500 lg:p-0 dark:text-white cursor-pointer flex items-center gap-1"
+                        aria-current="page">{{ $menuItem->title }} <x-eva-arrow-ios-downward class="w-3 h-3" x-show="!open" /><x-eva-arrow-ios-upward class="w-3 h-3" x-show="open" /></span>
+                        <ul class="absolute top-full left-0 flex flex-col font-medium p-4 bg-white border border-blue-500 dark:bg-gray-700 dark:border-gray-600 rounded gap-1 z-60" x-show="open">
+                            @foreach ($menuItem->children as $child)
 
 
-                        @php
-                            $hasLinkable = $child->linkable ?? null;
-                            $url = $hasLinkable && $child->linkable_type == 'App\Models\Page' ? Z3d0X\FilamentFabricator\Facades\FilamentFabricator::getPageUrlFromId($child->linkable->id) : $child->url;
-                        @endphp
+                            @php
+                                $hasLinkable = $child->linkable ?? null;
+                                $url = $hasLinkable && $child->linkable_type == 'App\Models\Page' ? Z3d0X\FilamentFabricator\Facades\FilamentFabricator::getPageUrlFromId($child->linkable->id) : $child->url;
+                            @endphp
 
-                            <li>
-                                <a href="{{ $url }}" wire:navigate
-                                    class="block py-2 pr-4 pl-3 text-sm rounded lg:p-0 text-slate-500 hover:text-blue-500 whitespace-nowrap"
-                                    aria-current="page">{{ $child->title }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </span>
-            @else
-                @php
-                    $hasLinkable = $menuItem->linkable ?? null;
-                    $url = $hasLinkable && $menuItem->linkable_type == 'App\Models\Page' ? Z3d0X\FilamentFabricator\Facades\FilamentFabricator::getPageUrlFromId($menuItem->linkable->id) : $menuItem->url;
-                @endphp
-                <a href="{{ $url }}" wire:navigate
-                    class="block py-2 pr-4 pl-3 text-sm text-primary-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                    aria-current="page">{{ $menuItem->title }}</a>
-            @endif
-        </li>
-        
-    @endforeach
-</ul>
+                                <li>
+                                    <a href="{{ $url }}" wire:navigate
+                                        class="block py-2 pr-4 pl-3 text-sm rounded lg:p-0 text-slate-500 hover:text-blue-500 whitespace-nowrap"
+                                        aria-current="page">{{ $child->title }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </span>
+                @else
+                    @php
+                        $hasLinkable = $menuItem->linkable ?? null;
+                        $url = $hasLinkable && $menuItem->linkable_type == 'App\Models\Page' ? Z3d0X\FilamentFabricator\Facades\FilamentFabricator::getPageUrlFromId($menuItem->linkable->id) : $menuItem->url;
+                    @endphp
+                    <a href="{{ $url }}" wire:navigate
+                        class="block py-2 pr-4 pl-3 text-sm text-primary-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                        aria-current="page">{{ $menuItem->title }}</a>
+                @endif
+            </li>
+            
+        @endforeach
+    </ul>
+</div>
