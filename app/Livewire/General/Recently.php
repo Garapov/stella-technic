5c44d6ecef9ebc6db30c2bc81e3d7e539af59da5
Stella-tech server <info@stella-technic.ru>
2025-09-17 00:33:13 +0300

    Блок недавно просмотренных товаров

--- app/Livewire/General/Recently.php
+++ app/Livewire/General/Recently.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\General;
 
+use App\Models\ProductVariant;
 use Livewire\Component;
+use Illuminate\Support\Collection;
 
 class Recently extends Component
 {
+    public $variations;
+
+    public function mount()
+    {
+        $this->variations = new Collection();
+    }
     public function render()
     {
-        return view('livewire.general.recently');
+        return view('livewire.general.recently', [
+            'variations' => $this->variations
+        ]);
+    }
+    public function loadProducts($productIds)
+    {
+        $this->variations = ProductVariant::whereIn('id', $productIds)->get();
     }
 }
--- resources/views/livewire/general/recently.blade.php
+++ resources/views/livewire/general/recently.blade.php
@@ -1,5 +1,7 @@
 <div>
     <section class="py-10 dark:bg-dark" x-data="{
+        products: $wire.$entangle('variations'),
+        isLoading: true,
         slider: new window.glide($refs.slider, {
             autoplay: 3000,
             perView: 5,
@@ -19,25 +21,32 @@
                     perView: 1
                 }
             }
-        }).mount(),
+        }),
         index: 0,
         init() {
             this.slider.on('move.after', () => {
                 this.index = this.slider.index;
             })
+            this.loadVariations();
         },
+        loadVariations() {
+            $wire.loadProducts($store.recently.list).then((products) => {
+                this.isLoading = false;
+                this.slider.mount();
+            });
+        }
     }">
         <div class="" x-ref="slider">
             <div class="flex items-center justify-between mb-10">
                 <p class="lg:text-4xl text-xl text-slate-900 dark:text-white font-semibold">Недавно просмотренные</p>
                 <div class="flex items-center gap-8">
                     <div class="flex items-center gap-2" data-glide-el="controls[nav]">
-                        {{-- @foreach ($variations as $key=>$variation)
+                        @foreach ($variations as $key=>$variation)
                             @if ($key > count($variations) - 5)
                                 @continue
                             @endif
                             <div class="h-2.5 rounded-full transition-width" :class="{'w-6 bg-blue-400': index == {{ $key }}, 'w-2.5 bg-gray-400': index != {{ $key }} }" data-glide-dir="={{ $key }}"></div>
-                        @endforeach --}}
+                        @endforeach
                     </div>
                     <div class="flex items-center gap-2" data-glide-el="controls">
                         <button type="button" class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 text-center inline-flex items-center dark:bg-blue-500 dark:hover:bg-blue-500 dark:focus:ring-blue-800" data-glide-dir="<">
@@ -52,11 +61,11 @@
             <div class="glide__track" data-glide-el="track">
                 <div class="glide__slides whitespace-normal">
 
-                    {{-- @foreach ($variations as $variation)
+                    @foreach ($variations as $variation)
                         @livewire('general.product-variant', [
                             'variant' => $variation
                         ], key($variation->id))
-                    @endforeach --}}
+                    @endforeach
                 </div>
             </div>
         </div>
