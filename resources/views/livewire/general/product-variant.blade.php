c79a7842b57a05ceef42408cbf8a003754301e0a
Ruslan Garapov <ruslangarapov1994@gmail.com>
2025-10-25 17:00:37 +0500

    Изменение кнопок добавления в  корзину в листинге

--- resources/views/livewire/general/product-variant.blade.php
+++ resources/views/livewire/general/product-variant.blade.php
@@ -1,5 +1,5 @@
 <div class="rounded-lg border border-gray-200 bg-white p-0 shadow-sm dark:border-gray-700 dark:bg-gray-900 relative flex flex-col overflow-hidden relative">
-    <div class="absolute left-2 right-2 top-2 z-2 flex items-center justify-between gap-2">
+    <div class="absolute left-2 right-2 top-2 z-10 flex items-center justify-between gap-2">
         
         <button type="button"
             @click.prevent="$store.favorites.toggleProduct({{ $variant->id }})"
@@ -139,41 +139,72 @@
             <div class="flex items-center gap-3">
                 
                 @if ($variant->count > 0)
-                <button class="rounded-lg p-2 text-blue-500 bg-white border-2 border-blue-500 flex-1 font-bold" @click="$store.application.forms.buy_one_click = true, $store.application.one_click_variation = {{json_encode($variant)}}">Купить в один клик</button>
-                    <button type="button"
-                    class="inline-flex items-center rounded-lg bg-green-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800" x-show="!$store.cart.list[{{ $variant->id }}]"
-                    @click="$store.cart.addVariationToCart({
-                        count: 1,
-                        variationId: {{ $variant->id }},
-                        name: '{{ $variant->name }}'
-                    });">
-                        <x-carbon-shopping-cart-plus class="h-5 w-5" />
-                    </button>
-
                     
 
 
-                    <div class="relative flex items-center" x-show="$store.cart.list[{{ $variant->id }}]">
-                        <button type="button" class="shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none" @click="() => {
-                            $store.cart.list[{{ $variant->id }}]--;
-                            if ($store.cart.list[{{ $variant->id }}] < 1) {
-                                $store.cart.list[{{ $variant->id }}] = 1
-                            }    
-                        }">
-                            <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
-                            </svg>
-                        </button>
-                        <input type="number" class="appearance-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none shrink-0 text-gray-900 dark:text-white border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-12 text-center" x-model="$store.cart.list[{{ $variant->id }}]" x-on:input.debounce="(event) => {
-                            if (event.target.value == '' || $store.cart.list[{{ $variant->id }}] < 1) $store.cart.list[{{ $variant->id }}] = 1;
 
-                            console.log($store.cart.list[{{ $variant->id }}]);
-                        }" />
-                        <button type="button" class="shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none" @click="() => { $store.cart.list[{{ $variant->id }}]++ }">
-                            <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
+
+
+                    <div class="relative" x-data="{ showTooltip: false }" @click="showTooltip = !showTooltip"
+                            @click.outside="showTooltip = false">
+                        <button type="button"
+                            
+                            class="inline-flex items-center rounded-lg bg-green-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800">
+                            <span class="sr-only">Купить</span>
+                            <x-carbon-shopping-cart-plus class="h-5 w-5" />
                         </button>
+                        <div x-show="showTooltip"
+                            x-cloak
+                            x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="opacity-0 translate-y-1"
+                            x-transition:enter-end="opacity-100 translate-y-0"
+                            x-transition:leave="transition ease-in duration-150"
+                            x-transition:leave-start="opacity-100 translate-y-0"
+                            x-transition:leave-end="opacity-0 translate-y-1"
+                            class="absolute bottom-full right-0 z-10 mb-2 rounded-md bg-slate-100 shadow px-3 py-2 text-sm font-medium text-white shadow-sm whitespace-nowrap">
+                            <div class="flex gap-2">
+                                <button class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 border border-blue-700 inset-ring inset-ring-blue-700/10" @click.stop="$store.application.forms.buy_one_click = true, $store.application.one_click_variation = {{json_encode($variant)}}">Купить в один клик</button>
+
+
+
+                                <button type="button"
+                                    class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 border border-green-700 inset-ring inset-ring-green-600/20" x-show="!$store.cart.list[{{ $variant->id }}]"
+                                    @click.stop="$store.cart.addVariationToCart({
+                                        count: 1,
+                                        variationId: {{ $variant->id }},
+                                        name: '{{ $variant->name }}'
+                                    });">
+                                        В корзину
+                                    </button>
+
+                                    
+
+
+                                    <div class="relative flex items-center" x-show="$store.cart.list[{{ $variant->id }}]">
+                                        <button type="button" class="shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none" @click.stop="() => {
+                                            $store.cart.list[{{ $variant->id }}]--;
+                                            if ($store.cart.list[{{ $variant->id }}] < 1) {
+                                                $store.cart.list[{{ $variant->id }}] = 1
+                                            }    
+                                        }">
+                                            <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
+                                            </svg>
+                                        </button>
+                                        <input type="number" class="appearance-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none shrink-0 text-gray-900 dark:text-white border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-12 text-center" @focus.stop x-model="$store.cart.list[{{ $variant->id }}]" x-on:input.debounce="(event) => {
+                                            if (event.target.value == '' || $store.cart.list[{{ $variant->id }}] < 1) $store.cart.list[{{ $variant->id }}] = 1;
+
+                                            console.log($store.cart.list[{{ $variant->id }}]);
+                                        }" />
+                                        <button type="button" class="shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none" @click.stop="() => { $store.cart.list[{{ $variant->id }}]++ }">
+                                            <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                                            </svg>
+                                        </button>
+                                    </div>
+                            </div>
+                            <div class="absolute -bottom-1 right-2 h-2 w-2 rotate-45 bg-slate-100 shadow"></div>
+                        </div>
                     </div>
                 @endif
                 @if ($variant->count < 1)
