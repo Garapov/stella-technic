2f1b0b7bf7ad07b1548ad8277856aab36d5cfa4f
Ruslan Garapov <ruslangarapov1994@gmail.com>
2025-11-01 18:34:23 +0500

    Рефакторинг каталога

--- resources/views/client/catalog.blade.php
+++ resources/views/client/catalog.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <livewire:catalog.items :path="$path" :display_filter="true"  />
+    {{-- <livewire:catalog.items-lazy :path="$path" :display_filter="true"  /> --}}
     <livewire:main.articles />
     <livewire:main.customers />
     <livewire:main.news />
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -3,14 +3,14 @@
     @seo(['description' => '✔ Стелла Техник: все для склада, складское оборудование, техника в Москве ✔ Продажа оборудования для склада от ведущих брендов ✔ Низкие тарифы на доставку по РФ ✔ Большие запасы и Гарантия.'])
     @seo(['image' => Storage::disk(config('filesystems.default'))->url(setting('site_logo'))])
     @seo(['url' => route('client.index')])
-    <livewire:main.slider />
-    <livewire:main.features />
-    <livewire:main.brands />
-    <livewire:main.popular />
-    <livewire:main.certificates/>
-    <livewire:main.articles />
-    <livewire:main.customers />
-    <livewire:main.news />
+    <livewire:main.slider lazy="on-load" />
+    <livewire:main.features lazy="on-load" />
+    <livewire:main.brands lazy="on-load" />
+    <livewire:main.popular lazy="on-load" />
+    <livewire:main.certificates lazy="on-load" />
+    <livewire:main.articles lazy="on-load" />
+    <livewire:main.customers lazy="on-load" />
+    <livewire:main.news lazy="on-load" />
     <livewire:general.contacts />
 
     <x-floating-control-panel>
