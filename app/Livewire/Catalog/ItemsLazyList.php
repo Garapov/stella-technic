72ca6b14d3a1a940fd74335578ef83b295504448
Ruslan Garapov <ruslangarapov1994@gmail.com>
2025-11-26 05:14:19 +0500

    Сортировка по наличию

--- app/Livewire/Catalog/ItemsLazyList.php
+++ app/Livewire/Catalog/ItemsLazyList.php
@@ -2,10 +2,8 @@
 
 namespace App\Livewire\Catalog;
 
-use App\Livewire\Cart\Components\Product;
 use App\Models\ProductVariant;
 use App\Services\ProductSelector;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Lazy;
@@ -18,16 +16,17 @@ use Livewire\WithPagination;
 class ItemsLazyList extends Component
 {
     use WithPagination;
+
     public $category = null;
 
     #[Url()]
     public $filters = [];
 
     #[Url]
-    public $sort = "count:asc";
+    public $sort = 'count:desc';
 
     #[Url]
-    public $displayMode = "block";
+    public $displayMode = 'block';
 
     public $showSorting = false;
 
@@ -47,33 +46,33 @@ class ItemsLazyList extends Component
         // }
     }
 
-    #[On("filters-changed")]
+    #[On('filters-changed')]
     public function updateFilters($filters)
     {
         $this->filters = $filters;
 
         $this->resetPage();
     }
-    
+
     public function render()
     {
         // sleep(4);
         return view('livewire.catalog.items-lazy-list');
     }
-    
-    
+
     public function variationsBuilder()
     {
-        if (!$this->selector || !$this->category) {
+        if (! $this->selector || ! $this->category) {
             return collect();
         }
         if ($this->category) {
-            return ProductVariant::whereIn('id', 
-                Cache::rememberForever('catalog:all_products:' . $this->category->slug, function () {
+            return ProductVariant::whereIn('id',
+                Cache::rememberForever('catalog:all_products:'.$this->category->slug, function () {
                     return $this->selector->fromCategory($this->category)->where('is_hidden', false)->pluck('id')->toArray();
                 })
             )->filter($this->filters)->sort([$this->sort])->with('parametrs');
         }
+
         return collect();
     }
 
@@ -94,31 +93,30 @@ class ItemsLazyList extends Component
 
         return $batches;
     }
-    
 
     #[Computed()]
     public function sortOptions()
     {
         return [
-            "count:asc" => [
-                "label" => "По умолчанию",
-                "icon" => "M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25",
+            'count:desc' => [
+                'label' => 'По умолчанию',
+                'icon' => 'M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25',
             ],
-            "price:asc" => [
-                "label" => "Подешевле",
-                "icon" => "M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12",
+            'price:asc' => [
+                'label' => 'Подешевле',
+                'icon' => 'M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12',
             ],
-            "price:desc" => [
-                "label" => "Подороже",
-                "icon" => "M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12",
+            'price:desc' => [
+                'label' => 'Подороже',
+                'icon' => 'M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12',
             ],
-            "name:asc" => [
-                "label" => "По названию А-Я",
-                "icon" => "M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25",
+            'name:asc' => [
+                'label' => 'По названию А-Я',
+                'icon' => 'M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25',
             ],
-            "name:desc" => [
-                "label" => "По названию Я-А",
-                "icon" => "M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25",
+            'name:desc' => [
+                'label' => 'По названию Я-А',
+                'icon' => 'M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25',
             ],
         ];
     }
@@ -142,8 +140,6 @@ class ItemsLazyList extends Component
 
         // $this->js('location.reload()');
     }
-
-    
 
     public function placeholder()
     {
