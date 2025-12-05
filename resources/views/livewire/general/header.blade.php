623e08a99af391bbd7c804eb80c7e22d5d9ecc66
Stella-tech server <info@stella-technic.ru>
2025-12-06 01:57:25 +0300

    скрытие всплывашки на главной

--- resources/views/livewire/general/header.blade.php
+++ resources/views/livewire/general/header.blade.php
@@ -124,7 +124,7 @@
     </header>
 
     <!-- Main modal -->
-    <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-2xl max-h-full ">
             <!-- Modal content -->
             <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6 bg-white">
